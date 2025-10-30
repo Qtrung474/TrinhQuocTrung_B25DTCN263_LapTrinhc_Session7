6f21b1f883104a0ec60778dd00317f6594ce69f2
Qtrung474 <biunon83@gmail.com>
2025-10-30 22:57:19 +0700

    09

--- 09.c
+++ 09.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a, b;
+    int timThay = 0;
+    printf("Nhap so nguyen duong n: ");
+    scanf("%d", &n);
+    printf("Cac cap so (a, b) thoa man:\n");
+    for (a = 0; a * a * a <= n; a++) {
+        for (b = 0; b * b * b <= n; b++) {
+            
+            if (a * a * a + b * b * b == n) {
+                printf("(%d, %d)\n", a, b);
+                timThay = 1;
+            }
+        }
+    }
+    if (timThay == 0) {
+        printf("Khong tim thay cap so nao.\n");
+    }
+    return 0;
+}
 
