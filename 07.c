72bc8de3f6e4494c3257d9be287544a707449a6e
Qtrung474 <biunon83@gmail.com>
2025-10-30 22:57:50 +0700

    07

--- 07.c
+++ 07.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
+#include <math.h> 
 
 int kiemTraSNT(int n) {
-    int i;
+    int i; 
+
     if (n < 2) {
-        return 0;
+        return 0; 
     }
-    for (i = 2; i < n; i++) {
+    for (i = 2; i <= sqrt(n); i++) { 
         if (n % i == 0) {
-            return 0;
+            return 0; 
         }
     }
-    return 1;
+    return 1; 
 }
 int main() {
     int a, b, i;
+
     printf("Nhap so nguyen a: ");
     scanf("%d", &a);
     printf("Nhap so nguyen b: ");
     scanf("%d", &b);
+    if (a > b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
     printf("Cac so nguyen to trong khoang [%d, %d] la: ", a, b);
     for (i = a; i <= b; i++) {
-        if (kiemTraSNT(i) == 1) {
+        if (kiemTraSNT(i) == 1) { 
             printf("%d ", i);
         }
     }
